52da9e089b99b9801d90cd21d7e92fcfb727f7d6
marcela-meira <marcelameiracarvalho@gmail.com>
2025-10-06 20:57:44 -0300

    Update peer.py

--- peer.py
+++ peer.py
@@ -12,14 +12,14 @@ RESOURCE_MAX_TIME=10  #ACHO Q DA P GENTE USAR ESSA MACRO P CONTROLAR O TEMPO DO 
 RESPONSE_MAX_TIME=20
 
 ''' PROBLEMA ATUAL: quando solicita um recurso os peers ficam travados, nunca retornando ao menu principal
-    Segundo Gemini: Em resumo, a lógica atual cria um ciclo de espera: o Peer8 solicita, o PeerA recebe e aprova, 
-    mas o PeerA fica travado esperando algo. Quando o Peer8 libera o recurso, ele tenta "despertar" o PeerA chamando solicitar_recurso(),
+    Segundo Gemini: Em resumo, a lógica atual cria um ciclo de espera: o PeerB solicita, o PeerA recebe e aprova, 
+    mas o PeerA fica travado esperando algo. Quando o PeerB libera o recurso, ele tenta "despertar" o PeerA chamando solicitar_recurso(),
     o que só agrava o impasse. A solução é que o PeerA receba uma notificação simples, permitindo que ele mesmo, de forma independente, continue seu processo.
     Acho que é aquela lógica que a gente tinha visto antes no chat de enviar um 'OK' e o próprio peer chamar seus métodos e não um peer chamar remotamente o 
     método para o outro.
 '''
 
-''' thread pra escutar heartbeat - time.tempo_atual menos ultimo heartbeat se for maior que temporizador = nó falhou (fazer para todos os peers)
+''' thread pra escutar heartbeat -> time.tempo_atual menos ultimo heartbeat se for maior que temporizador = nó falhou (fazer para todos os peers)
     alternativa: ter um threading timer que ativa a checagem acima de tempos em tempos
                 marca quando recebe e de quem o heartbeat
                 timer checa no tempo (temporizador + uns ms) se recebeu de todos
@@ -48,6 +48,7 @@ class Peer(object):
 
     def remover_processo(self,peer_name):
         # O peer é removido da lista local de peers ativos
+        print(f"Removendo {peer_name} por inatividade ")
         self.peers_ativos.remove(peer_name)
 
     def solicitar_recurso(self):
@@ -59,6 +60,8 @@ class Peer(object):
         contador=0
         ns = Pyro5.api.locate_ns()  
         
+        self.peers_ativos.clear()
+
         for peer_name in list(ns.list().keys())[1:]:         
             if peer_name==self.name:
                 continue
@@ -68,31 +71,36 @@ class Peer(object):
             self.peers_ativos.append(peer_name)
 
             print(f"Solicitando recurso para {peer_name}")
-            resposta=peer.receber_pedido(self.name, self.my_request_timestamp)
             self.response_time = threading.Timer(RESPONSE_MAX_TIME,self.remover_processo, args=[peer_name])
             self.response_time.start()
 
+            resposta=peer.receber_pedido(self.name, self.my_request_timestamp)
+            
             if resposta==True:
                 contador+=1
-            else:
-               print(f"Você está na fila para acessar o recurso")  
+                self.response_time.cancel()
+            elif resposta==False:
+               self.response_time.cancel()
+               print(f"Você está na fila para acessar o recurso") 
 
-        if contador == ns.count()-2:
+        if contador == len(self.peers_ativos):
             print(f"\nVocê pode acessar o recurso")
             self.state="HELD"    
             self.usar_recurso()        
             return True
 
     # Colocar sleep para simular peer inativo
+    
+    #@Pyro5.api.oneway
     def receber_pedido(self, requester_name, request_timestamp):
         print(f"Solicitação recebida de {requester_name}")
         if (self.state=="WANTED" and self.my_request_timestamp < request_timestamp) or self.state=="HELD":
             self.request_queue.append((requester_name, request_timestamp))
             self.request_queue.sort(key=lambda x: (x[1], x[0]))
-            print("\nSolicitação não aprovada")
+            print(f"\nSolicitação não aprovada para {requester_name}")
             return False
         else:
-           print("\nSolicitação aprovada")
+           print(f"\nSolicitação aprovada para {requester_name}")
            return True
         ## Simulação de peer inativo
         #time.sleep(30)
@@ -105,15 +113,16 @@ class Peer(object):
 
             self.state="RELEASED"
             self.my_request_timestamp=0
-            print(f"Recurso liberado. Respondendo à fila.")
+            print(f"Recurso liberado.")
 
         ns = Pyro5.api.locate_ns()  
         for requester_name,_ in self.request_queue:
             uri = ns.lookup(requester_name)
             requester_proxy = Pyro5.api.Proxy(uri)
             print(f"Enviando permissão tardia para {requester_name}")
-            requester_proxy.solicitar_recurso()             
-        self.request_queue.clear()
+            requester_proxy.solicitar_recurso()
+        '''if self.request_queue :
+            self.request_queue.clear()'''
 
 def main():
 
@@ -140,8 +149,8 @@ def main():
         )
 
         if opcao == "1":
-            print("Solicitar recurso\n")           
-            peer.solicitar_recurso()
+            print("Solicitar recurso\n")   
+            threading.Thread(target=peer.solicitar_recurso, daemon=True).start()
         elif opcao == "2":
             print("Listar peers ativos")
             print(ns.list())  # exemplo: mostra peers registrados
